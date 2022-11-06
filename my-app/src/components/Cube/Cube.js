17ed6fb01e481a65af01a7b1e4803099d1964704
Thomas Kenny <thomaskenny9734@gmail.com>
2022-11-06 13:48:43 -0500

    added onClick event to all spans and removed movement on hover

--- my-app/src/components/Cube/Cube.js
+++ my-app/src/components/Cube/Cube.js
@@ -22,52 +22,52 @@ function Cube() {
       <div className='cube'>
         <div style={{'--x': -1, '--y':0}}>
           <span onClick={(e) => moveCubeOnClick(e)} style={{'--i': 3}}></span>
-          <span style={{'--i': 2}}></span>
-          <span style={{'--i': 1}}></span>
+          <span onClick={(e) => moveCubeOnClick(e)} style={{'--i': 2}}></span>
+          <span onClick={(e) => moveCubeOnClick(e)} style={{'--i': 1}}></span>
         </div>
         <div style={{'--x': 0, '--y':0}}>
-          <span style={{'--i': 3}}></span>
-          <span style={{'--i': 2}}></span>
-          <span style={{'--i': 1}}></span>
+          <span onClick={(e) => moveCubeOnClick(e)} style={{'--i': 3}}></span>
+          <span onClick={(e) => moveCubeOnClick(e)} style={{'--i': 2}}></span>
+          <span onClick={(e) => moveCubeOnClick(e)} style={{'--i': 1}}></span>
         </div>
         <div style={{'--x': 1, '--y':0}}>
-          <span style={{'--i': 3}}></span>
-          <span style={{'--i': 2}}></span>
-          <span style={{'--i': 1}}></span>
+          <span onClick={(e) => moveCubeOnClick(e)} style={{'--i': 3}}></span>
+          <span onClick={(e) => moveCubeOnClick(e)} style={{'--i': 2}}></span>
+          <span onClick={(e) => moveCubeOnClick(e)} style={{'--i': 1}}></span>
         </div>
       </div>
       <div className='cube'>
         <div style={{'--x': -1, '--y':0}}>
-          <span style={{'--i': 3}}></span>
-          <span style={{'--i': 2}}></span>
-          <span style={{'--i': 1}}></span>
+          <span onClick={(e) => moveCubeOnClick(e)} style={{'--i': 3}}></span>
+          <span onClick={(e) => moveCubeOnClick(e)} style={{'--i': 2}}></span>
+          <span onClick={(e) => moveCubeOnClick(e)} style={{'--i': 1}}></span>
         </div>
         <div style={{'--x': 0, '--y':0}}>
-          <span style={{'--i': 3}}></span>
-          <span style={{'--i': 2}}></span>
-          <span style={{'--i': 1}}></span>
+          <span onClick={(e) => moveCubeOnClick(e)} style={{'--i': 3}}></span>
+          <span onClick={(e) => moveCubeOnClick(e)} style={{'--i': 2}}></span>
+          <span onClick={(e) => moveCubeOnClick(e)} style={{'--i': 1}}></span>
         </div>
         <div style={{'--x': 1, '--y':0}}>
-          <span style={{'--i': 3}}></span>
-          <span style={{'--i': 2}}></span>
-          <span style={{'--i': 1}}></span>
+          <span onClick={(e) => moveCubeOnClick(e)} style={{'--i': 3}}></span>
+          <span onClick={(e) => moveCubeOnClick(e)} style={{'--i': 2}}></span>
+          <span onClick={(e) => moveCubeOnClick(e)} style={{'--i': 1}}></span>
         </div>
       </div>
       <div className='cube'>
         <div style={{'--x': -1, '--y':0}}>
-          <span style={{'--i': 3}}></span>
-          <span style={{'--i': 2}}></span>
-          <span style={{'--i': 1}}></span>
+          <span onClick={(e) => moveCubeOnClick(e)} style={{'--i': 3}}></span>
+          <span onClick={(e) => moveCubeOnClick(e)} style={{'--i': 2}}></span>
+          <span onClick={(e) => moveCubeOnClick(e)} style={{'--i': 1}}></span>
         </div>
         <div style={{'--x': 0, '--y':0}}>
-          <span style={{'--i': 3}}></span>
-          <span style={{'--i': 2}}></span>
-          <span style={{'--i': 1}}></span>
+          <span onClick={(e) => moveCubeOnClick(e)} style={{'--i': 3}}></span>
+          <span onClick={(e) => moveCubeOnClick(e)} style={{'--i': 2}}></span>
+          <span onClick={(e) => moveCubeOnClick(e)} style={{'--i': 1}}></span>
         </div>
         <div style={{'--x': 1, '--y':0}}>
-          <span style={{'--i': 3}}></span>
-          <span style={{'--i': 2}}></span>
-          <span style={{'--i': 1}}></span>
+          <span onClick={(e) => moveCubeOnClick(e)} style={{'--i': 3}}></span>
+          <span onClick={(e) => moveCubeOnClick(e)} style={{'--i': 2}}></span>
+          <span onClick={(e) => moveCubeOnClick(e)} style={{'--i': 1}}></span>
         </div>
       </div>
     </div>
